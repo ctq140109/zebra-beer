67b1d856d075af4083bf130d5f7e7984016c53b0
ctq140109 <1401095999@qq.com>
2019-06-10 02:02:41 +0800

    new

--- service/orders.js
+++ service/orders.js
@@ -37,6 +37,32 @@ class OrdersModel extends HTTP {
       })
     }
   }
+  //退款
+  refund(out_trade_no, total_fee) {
+    let fee = total_fee * 100; //转换为分为单位
+    return this.request({
+      url: '/BeerApp/wx/refund.do?out_trade_no=' + out_trade_no + '&total_fee=' + fee,
+      method: 'POST',
+      header: 'json'
+    })
+  }
+  //取消订单
+  cancelOrder(id) {
+    return this.request({
+      url: '/BeerApp/trade/cancel.do?id=' + id,
+      method: 'POST',
+      header: 'json'
+    })
+  }
+  //获取订单分类数量
+  getOrderNum() {
+    let openid = wx.getStorageSync("openid");
+    if (openid != '') {
+      return this.request({
+        url: '/BeerApp/trade/statistics?userId=' + openid
+      })
+    }
+  }
 }
 export {
   OrdersModel
