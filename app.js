f83021de9430fc0a5bc24fbec422aaafc30585cf
ctq140109 <1401095999@qq.com>
2019-07-11 18:37:44 +0800

    时间判断

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ App({
   onLaunch: function() {
     console.log('app初始化');
     let http = new HTTP();
-    // http.request({
-    //   url: ''
-    // }).then(res => {
-    //   console.log(res);
-    // })
+    //获取营业时间
+    http.request({
+      url: '/BeerApp/shopTime/get.do'
+    }).then(res => {
+      console.log(res.data);
+      wx.setStorageSync("shopTime", JSON.stringify(res.data));
+    })
   },
   globalData: {
     userInfo: null,
